b687b16386a957f035b3de3971ae965da734a61d
Aneesh Anirudhan <aneesh.anirudhan@centrica.com>
2018-05-25 14:44:50 +0100

    removing merging the public folder

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 /* eslint-env node */
 'use strict';
 
-const Funnel        = require('broccoli-funnel');
-const mergeTrees    = require('broccoli-merge-trees');
-
 module.exports = {
   name: 'ember-calendar-button',
   isDevelopingAddon: function () {
     return true;
-  },
-  treeForPublic: function(tree) {
-    var assetsTree = new Funnel('public');
-    return mergeTrees([tree, assetsTree], {
-      overwrite: true
-    });
   }
 };
