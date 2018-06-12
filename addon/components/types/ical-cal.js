165c4a6480796c3f073492ffae9f62325add1db2
Aneesh Anirudhan <aneesh.anirudhan@centrica.com>
2018-06-12 10:52:48 +0100

    escaping carriage return and new line constant for outlook description

--- addon/components/types/ical-cal.js
+++ addon/components/types/ical-cal.js
@@ -39,7 +39,7 @@ export default Base.extend({
           `DTSTART;TZID=Europe/London:${start}`,
           `DTEND;TZID=Europe/London:${end}`,
           `SUMMARY:${title}`,
-          `DESCRIPTION:${plainDescription.replace('\n', '\\n')}`,
+          `DESCRIPTION:${plainDescription.replace('\r','\\r').replace('\n', '\\n')}`,
           `LOCATION:${location}`,
           'END:VEVENT',
           'END:VCALENDAR'
